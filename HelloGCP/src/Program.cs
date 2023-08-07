4c41395d7b4d368a1e8ba40e2e34e729b776e094
Ed Devera <eddevera@hotmail.com>
2023-08-07 16:03:12 -0700

    uh

--- HelloGCP/src/Program.cs
+++ HelloGCP/src/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Google.Api;
+using Google.Api.Gax.Grpc;
+using Google.Cloud.Logging.V2;
+
+Console.WriteLine("Hello, World!");
+
+// Log to the global log in GCP using Application Default Credentials.
+var loggingClient = LoggingServiceV2Client.Create();
+LogEntry logEntry = new()
+{
+    TextPayload = "Hello GCP!",
+    Resource = MonitoredResourceBuilder.GlobalResource
+};
+WriteLogEntriesRequest logRequest = new()
+{
+    LogName = "projects/gcplearning-hellogcp/logs/hellogcp"
+};
+logRequest.Entries.Add(logEntry);
+loggingClient.WriteLogEntries(logRequest);
+
+Console.WriteLine("Done :)");
